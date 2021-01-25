cd659de423478c9676489fb949ef14069d9c2e78
Juan Sanchez <juan.sanchez@phila.gov>
2021-01-25 03:52:07 -0500

    working current day function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
   let DateTime = luxon.DateTime;
-  let today = DateTime.local();
+  let today = DateTime.local().toLocaleString(DateTime.DATE_HUGE);
   $("#currentDay").text(today);
-})
+});
